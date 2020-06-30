c26e8f40d6855e1da1fbda74f67bda108c6d007e
Streeterxs <afonso_mg3@hotmail.com>
2020-06-30 00:05:15 -0300

    Relay - Shared relay code: created library for shared relay code. Firstly a services that has a environmentModule, that module take care of creation of a environment for a relay client.

---
+++ packages/relay/index.d.ts
@@ -0,0 +1,45 @@
+import {default as environmentModule} from './distTs';
+import { Environment } from 'relay-runtime/lib/store/RelayStoreTypes';
+
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+
+/*~ This is the module template file for function modules.
+ *~ You should rename it to index.d.ts and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+
+// Note that ES6 modules cannot directly export class objects.
+// This file should be imported using the CommonJS-style:
+//   import x = require('[~THE MODULE~]');
+//
+// Alternatively, if --allowSyntheticDefaultImports or
+// --esModuleInterop is turned on, this file can also be
+// imported as a default import:
+//   import x from '[~THE MODULE~]';
+//
+// Refer to the TypeScript documentation at
+// https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
+// to understand common workarounds for this limitation of ES6 modules.
+
+
+/*~ This declaration specifies that the function
+ *~ is the exported object from the file
+ */
+export = environmentModule;
+
+/*~ This example shows how to have multiple overloads for your function */
+declare function environmentModule(url: string, auth: string): Environment;
+
+/*~ If you want to expose types from your module as well, you can
+ *~ place them in this block. Often you will want to describe the
+ *~ shape of the return type of the function; that type should
+ *~ be declared in here, as this example shows.
+ *~
+ *~ Note that if you decide to include this namespace, the module can be
+ *~ incorrectly imported as a namespace object, unless
+ *~ --esModuleInterop is turned on:
+ *~   import * as x from '[~THE MODULE~]'; // WRONG! DO NOT DO THIS!
+ */
