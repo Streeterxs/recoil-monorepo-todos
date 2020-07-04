3efe23370c13b4a8ec6fbaf5872a0f8421e61cd2
Streeterxs <afonso_mg3@hotmail.com>
2020-07-04 20:33:44 -0300

    Relay - useTodoUpdateMutation: Created hook to update todo with a mutation.

---
+++ packages/relay/src/Hooks/useTodoUpdateMutation.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useCallback } from 'react';
+import { graphql, Disposable } from 'react-relay';
+import { useMutation } from 'react-relay/hooks';
+
+// to generate this file, yarn relay:build root package.json script must be executed
+import { useTodoUpdateMutation } from './__generated__/useTodoUpdateMutation.graphql';
+
+
+const todoUpdateMutation = graphql`
+    mutation useTodoUpdateMutation($content: String!, $id: String!) {
+        TodosUpdate (input: {content: $content, id: $id, clientMutationId: "3"}) {
+            todo {
+                cursor
+                node {
+                    id
+                    content
+                    createdAt
+                    updatedAt
+                }
+            }
+        }
+    }
+`;
+
+const useTodoUpdateMutationHook = () => {
+    const useTodoUpdateMutationCallback = useCallback(() => useMutation<useTodoUpdateMutation>(todoUpdateMutation), []);
+
+    useEffect(() => {
+        console.log('useEffect useTodoUpdateMutation');
+    });
+
+    return useTodoUpdateMutationCallback;
+
+};
+
+export default useTodoUpdateMutationHook;
--- packages/relay/src/Hooks/userCreationMutation.tsx
+++ packages/relay/src/Hooks/userCreationMutation.tsx
@@ -2,12 +2,11 @@ import React, { useEffect, useCallback } from 'react';
 import { graphql, Disposable } from 'react-relay';
 import { useMutation } from 'react-relay/hooks';
 
-import { UseMutationConfig } from 'react-relay/lib/relay-experimental/useMutation';
 // to generate this file, yarn relay:build root package.json script must be executed
-import { useCreationMutation } from './__generated__/useCreationMutation.graphql';
+import { userCreationMutation } from './__generated__/userCreationMutation.graphql';
 
 
-const userCreationMutation = graphql`
+const userCreationMutationGraphQL = graphql`
     mutation userCreationMutation($identifier: String!) {
         UserCreation (input: {identifier: $identifier, clientMutationId: "1"}) {
             user {
@@ -20,7 +19,7 @@ const userCreationMutation = graphql`
 `;
 
 const userCreationMutationHook = () => {
-    const userCreationMutationCallback = useCallback(() => useMutation<useCreationMutation>(userCreationMutation), []);
+    const userCreationMutationCallback = useCallback(() => useMutation<userCreationMutation>(userCreationMutationGraphQL), []);
 
     useEffect(() => {
         console.log('useEffect useCreationMutation');
